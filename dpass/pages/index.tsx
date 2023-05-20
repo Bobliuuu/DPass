407fa125b66fa8d68c19f973178a6703f5867e57
Ri-Hong <riri.hong@gmail.com>
2023-05-20 00:16:49 -0400

    added basic frontend

--- dpass/pages/index.tsx
+++ dpass/pages/index.tsx
@@ -1,28 +1,36 @@
 import Head from 'next/head'
 import clientPromise from '../lib/mongodb'
 import { InferGetServerSidePropsType } from 'next'
+import Layout from '../components/layout';
+import NestedLayout from '../components/nested-layout';
+import Link from 'next/link';
+
 
 export async function getServerSideProps(context) {
-  try {
-    await clientPromise
-    // `await clientPromise` will use the default database passed in the MONGODB_URI
-    // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
-    //
-    // `const client = await clientPromise`
-    // `const db = client.db("myDatabase")`
-    //
-    // Then you can execute queries against your database like so:
-    // db.find({}) or any of the MongoDB Node Driver commands
-
-    return {
-      props: { isConnected: true },
-    }
-  } catch (e) {
-    console.error(e)
-    return {
-      props: { isConnected: false },
-    }
+  return {
+    props: { isConnected: true },
   }
+
+  // try {
+  //   await clientPromise
+  //   // `await clientPromise` will use the default database passed in the MONGODB_URI
+  //   // However you can use another database (e.g. myDatabase) by replacing the `await clientPromise` with the following code:
+  //   //
+  //   // `const client = await clientPromise`
+  //   // `const db = client.db("myDatabase")`
+  //   //
+  //   // Then you can execute queries against your database like so:
+  //   // db.find({}) or any of the MongoDB Node Driver commands
+
+  //   return {
+  //     props: { isConnected: true },
+  //   }
+  // } catch (e) {
+  //   console.error(e)
+  //   return {
+  //     props: { isConnected: false },
+  //   }
+  // }
 }
 
 export default function Home({
@@ -35,56 +43,17 @@ export default function Home({
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js with MongoDB!</a>
-        </h1>
-
-        {isConnected ? (
-          <h2 className="subtitle">You are connected to MongoDB</h2>
-        ) : (
-          <h2 className="subtitle">
-            You are NOT connected to MongoDB. Check the <code>README.md</code>{' '}
-            for instructions.
-          </h2>
-        )}
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/filePage">File</Link>
+        </li>
+        <li>
+          <Link href="/blog/hello-world">Blog Post</Link>
+        </li>
+      </ul>
 
       <footer>
         <a
